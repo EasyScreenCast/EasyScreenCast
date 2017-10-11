7b12e8743b9a47cbd76949a3b8ff272ea5c53c2a
IacopoDeeNosee <iacopodeenosee@gmail.com>
2017-10-11 10:05:21 +0200

    change button to spinner to get log, simplify UI

--- prefs.js
+++ prefs.js
@@ -84,7 +84,7 @@ const EasyScreenCastSettingsWidget = new GObject.Class({
             this._initTabFile(this, builder, Settings.settings);
 
             // setup tab support
-            this._initTabSupport(this, builder);
+            this._initTabSupport(this, builder, Settings.settings);
 
             // setup tab info
             this._initTabInfo(this, builder);
@@ -195,13 +195,6 @@ const EasyScreenCastSettingsWidget = new GObject.Class({
         tmpS.bind(Settings.POST_CMD_SETTING_KEY,
             Ref_textedit_PostCMD,
             'text', Gio.SettingsBindFlags.DEFAULT);
-
-        //implements verbose debug option
-        let Ref_switch_VerboseDebug = gtkDB.get_object(
-            'swt_VerboseDebug');
-        tmpS.bind(Settings.VERBOSE_DEBUG_SETTING_KEY,
-            Ref_switch_VerboseDebug,
-            'active', Gio.SettingsBindFlags.DEFAULT);
     },
 
     _initTabQuality(ctx, gtkDB, tmpS){
@@ -658,96 +651,114 @@ const EasyScreenCastSettingsWidget = new GObject.Class({
 
     },
 
-    _initTabSupport(ctx, gtkDB){
-        // implements textentry log
+    _initTabSupport(ctx, gtkDB, tmpS){
+        //implements textentry log
         let Ref_TextView_ESCLog = gtkDB.get_object('txe_ContainerLog');
         let Ref_buffer_Log = Ref_TextView_ESCLog.get_buffer();
 
-        //implements get extension log button action
-        let Ref_button_GetExtLog = gtkDB.get_object(
-            'btn_GetExtLog');
-        Ref_button_GetExtLog.connect('clicked',
-            () => {
-                //clear buffer
-                Ref_buffer_Log.delete(
-                    Ref_buffer_Log.get_start_iter(),
-                    Ref_buffer_Log.get_end_iter());
+        //implements verbose debug option
+        let Ref_switch_VerboseDebug = gtkDB.get_object(
+            'swt_VerboseDebug');
+        tmpS.bind(Settings.VERBOSE_DEBUG_SETTING_KEY,
+            Ref_switch_VerboseDebug,
+            'active', Gio.SettingsBindFlags.DEFAULT);
 
-                ctx.CtrlExe.Execute('journalctl --since "15 min ago" --output=cat --no-pager',
-                    false, (success)=>{
-                        Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
-                    },
-                    (line)=>{
-                        let esc = line.indexOf("[ESC]");
-                        if(line !=='' && line !== undefined && esc !== -1){
-                            line += '\n';
-                            Ref_buffer_Log.insert(
-                                Ref_buffer_Log.get_end_iter(),
-                                line,
-                                line.length);
-                        }
-                    });
+         Ref_switch_VerboseDebug.connect('state-set',
+            (self) => {
+                //update log display widgets
+                Ref_TextView_ESCLog.sensistive = self.active;
+                Ref_combobox_LogChooser.sensitive = self.active;
             });
 
-        //implements get gnome log button action
-        let Ref_button_GetGnomeLog = gtkDB.get_object(
-            'btn_GetGnomeLog');
-        Ref_button_GetGnomeLog.connect('clicked',
-            () => {
-                //clear buffer
-                Ref_buffer_Log.delete(
-                    Ref_buffer_Log.get_start_iter(),
-                    Ref_buffer_Log.get_end_iter());
+        //implements file resolution preset spinner
+        let Ref_combobox_LogChooser = gtkDB.get_object(
+            'cbt_LogChooser');
 
-                ctx.CtrlExe.Execute('journalctl /usr/bin/gnome-shell --since "15 min ago" --output=cat --no-pager',
-                    false, (success)=>{
-                        Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
-                    },
-                    (line)=>{
-                        if(line !=='' && line !== undefined){
-                            line += '\n';
-                            Ref_buffer_Log.insert(
-                                Ref_buffer_Log.get_end_iter(),
-                                line,
-                                line.length);
-                        }
-                    });
-            });
+        //intercept combobox res changed and update width/height value
+        Ref_combobox_LogChooser.connect('changed',
+            (self) => {
+                var activeLog = self.active;
+                Lib.TalkativeLog('-^-log combobox changed: ' + activeLog);
+                switch(activeLog){
+                case 0:
+                    //clear buffer
+                    Ref_buffer_Log.delete(
+                        Ref_buffer_Log.get_start_iter(),
+                        Ref_buffer_Log.get_end_iter());
 
-        //implements get last GSP button action
-        let Ref_button_GetLastGSP = gtkDB.get_object(
-            'btn_GetLastGSP');
-        Ref_button_GetLastGSP.connect('clicked',
-            () => {
-                //clear buffer
-                Ref_buffer_Log.delete(
-                    Ref_buffer_Log.get_start_iter(),
-                    Ref_buffer_Log.get_end_iter());
-
-                ctx.CtrlExe.Execute('journalctl --since "15 min ago" --output=cat --no-pager',
-                    false, (success)=>{
-                        Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
-                        if(success){
-                            if(Ref_buffer_Log.get_line_count() > 0){
-                                let strNOgsp = _('No Gstreamer pipeline found');
+                    ctx.CtrlExe.Execute('journalctl --since "15 min ago" --output=cat --no-pager',
+                        false, (success)=>{
+                            Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
+                        },
+                        (line)=>{
+                            let esc = line.indexOf("[ESC]");
+                            if(line !=='' && line !== undefined && esc !== -1){
+                                line += '\n';
                                 Ref_buffer_Log.insert(
                                     Ref_buffer_Log.get_end_iter(),
-                                    strNOgsp,
-                                    strNOgsp.length);
+                                    line,
+                                    line.length);
                             }
-                        }
-                    },
-                    (line)=>{
-                        let esc = line.indexOf('-§-final GSP :');
-                        if(line !=='' && line !== undefined && esc !== -1){
-                            line += '\n';
-                            Ref_buffer_Log.insert(
-                                Ref_buffer_Log.get_end_iter(),
-                                line,
-                                line.length);
-                        }
-                    });
+                        });
+                    break;
+                case 1:
+                    //clear buffer
+                    Ref_buffer_Log.delete(
+                        Ref_buffer_Log.get_start_iter(),
+                        Ref_buffer_Log.get_end_iter());
+
+                    ctx.CtrlExe.Execute('journalctl --since "15 min ago" --output=cat --no-pager',
+                        false, (success)=>{
+                            Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
+                            if(success){
+                                if(Ref_buffer_Log.get_line_count() > 0){
+                                    let strNOgsp = _('No Gstreamer pipeline found');
+                                    Ref_buffer_Log.insert(
+                                        Ref_buffer_Log.get_end_iter(),
+                                        strNOgsp,
+                                        strNOgsp.length);
+                                }
+                            }
+                        },
+                        (line)=>{
+                            let esc = line.indexOf('-§-final GSP :');
+                            if(line !=='' && line !== undefined && esc !== -1){
+                                line += '\n';
+                                Ref_buffer_Log.insert(
+                                    Ref_buffer_Log.get_end_iter(),
+                                    line,
+                                    line.length);
+                            }
+                        });
+                    break;
+                case 2:
+                    //clear buffer
+                    Ref_buffer_Log.delete(
+                        Ref_buffer_Log.get_start_iter(),
+                        Ref_buffer_Log.get_end_iter());
+
+                    ctx.CtrlExe.Execute('journalctl /usr/bin/gnome-shell --since "15 min ago" --output=cat --no-pager',
+                        false, (success)=>{
+                            Lib.TalkativeLog('-^-CALLBACK async S= '+ success);
+                        },
+                        (line)=>{
+                            if(line !=='' && line !== undefined){
+                                line += '\n';
+                                Ref_buffer_Log.insert(
+                                    Ref_buffer_Log.get_end_iter(),
+                                    line,
+                                    line.length);
+                            }
+                        });
+                    break;
+                default:
+                    break;
+                };
             });
+
+        //update state of get log
+        Ref_combobox_LogChooser.sensistive = Settings.getOption(
+            'b', Settings.VERBOSE_DEBUG_SETTING_KEY);
 
         //implements default button action
         let Ref_button_SetDeafaultSettings = gtkDB.get_object(
